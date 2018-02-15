b585ee02b5fd49d13348ff90b40a94b7f5b57c38
Eon <eoneon123@gmail.com>
2018-02-15 10:54:04 -0800

    added sign descriptive methods

--- app/models/sign_type.rb
+++ app/models/sign_type.rb
@@ -1,4 +1,24 @@
 class SignType < ApplicationRecord
   belongs_to :category
   has_many :items
+
+  def sign_description
+    case
+    when ["hand", "estate"].include?(properties["signmethod"]) && properties["signtype"].present? && properties["signer"].present? then signed_by
+    when ["plate", "authorized"].include?(properties["signmethod"]) && properties["signtype"].present? && properties["signer"].present? then bearing_signature
+    when properties["signtype"] == "not signed" then unsigned
+    end
+  end
+
+  def signed_by
+    [properties["signmethod"], properties["signtype"], "by the", properties["signer"]].join(" ")
+  end
+
+  def bearing_signature
+    ["bearing the", properties["signmethod"], "signature of the", properties["signer"]].join(" ")
+  end
+
+  def unsigned
+    ["This piece is unsiged."]
+  end
 end
