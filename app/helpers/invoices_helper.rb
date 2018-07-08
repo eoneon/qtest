4fc95fff0a8465d020768b78a3ab5f26732cf55d
Eon <eoneon123@gmail.com>
2018-07-08 16:08:10 -0700

    added methods to inv_helper for displaying sku ranges

--- app/helpers/invoices_helper.rb
+++ app/helpers/invoices_helper.rb
@@ -1,2 +1,75 @@
 module InvoicesHelper
+  def inv_skus
+    @invoice.items.order(:sku).pluck(:sku)
+  end
+
+  def sku_count
+    inv_skus.count
+  end
+
+  def first_sku
+    inv_skus[0]
+  end
+
+  def last_sku
+    inv_skus[-1]
+  end
+
+  def last_idx
+    inv_skus.index(last_sku)
+  end
+
+  def sku_count
+    inv_skus.count
+  end
+
+  def sequential_range?
+    last_sku - first_sku == last_idx
+  end
+
+  def next_sku_sequential?(idx)
+    inv_skus[idx] + 1 == inv_skus[idx + 1]
+  end
+
+  def prev_sku_sequential?(idx)
+    idx == 0 || inv_skus[idx] - 1 == inv_skus[idx -1]
+  end
+
+  def format_sku_rng(sku_rng)
+    [sku_rng[0], sku_rng[-1]].uniq.join("-")
+  end
+
+  def sequential_sub_range(h, idx)
+    sku_rng = []
+    inv_skus.drop(inv_skus.index(inv_skus[idx])).each do |sku|
+      abs_idx = inv_skus.index(sku)
+      if sku_rng.empty? || prev_sku_sequential?(abs_idx)
+        sku_rng << sku
+      end
+    end
+    h[:last] = sku_rng[-1]
+    format_sku_rng(sku_rng)
+  end
+
+  def format_skus
+    h = {skus: [], last: ""}
+    inv_skus.each.with_index do |sku, idx|
+      next if sku <= h[:last].to_i
+      if next_sku_sequential?(idx)
+        h[:skus] << sequential_sub_range(h, idx)
+      else
+        h[:skus] << sku
+        h[:last] = sku
+      end
+    end
+    h[:skus].join(", ")
+  end
+
+  def display_inv_skus
+    if sequential_range?
+      format_sku_rng([inv_skus[0], inv_skus[-1]])
+    else
+      format_skus
+    end
+  end
 end
--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,4 +1,12 @@
 module ItemsHelper
+  def inv_items
+    Item.where(invoice_id: @item.invoice_id).order(:sku)
+  end
+
+  def inv_skus
+    inv_items.pluck(:sku)
+  end
+
   def prev_item(idx, inv_items)
     idx == 0 ? inv_items[0] : inv_items[idx - 1]
   end
@@ -11,12 +19,49 @@ module ItemsHelper
     inv_items[0]
   end
 
+  # def last_item(inv_items)
+  #   inv_items[-1]
+  # end
+
+  ##
+  def first_sku
+    #inv_items[0].sku
+    inv_skus[0]
+  end
+
+  def last_sku
+    inv_skus[-1]
+  end
+
   def last_item(inv_items)
     inv_items[-1]
   end
 
+  def last_idx
+    inv_skus.index(last_sku)
+  end
+
+  def sku_count
+    inv_skus.count
+  end
+
+  def sequential_range?
+    last_sku - first_sku == last_idx
+  end
+
+  def next_sku_sequential?(idx)
+    inv_skus[idx] + 1 == inv_skus[idx + 1]
+  end
+
+  def prev_sku_sequential?(idx)
+    idx == 0 || inv_skus[idx] - 1 == inv_skus[idx -1]
+  end
+
+  def format_sku_rng(sku_rng)
+    [sku_rng[0], sku_rng[-1]].uniq.join("-")
+  end
+
   def sku_nav(item)
-    inv_items = Item.where(invoice_id: item.invoice_id).order(:sku)
     idx = inv_items.index(item)
     h = {prev_item: prev_item(idx, inv_items), next_item: next_item(idx, inv_items), first_item: first_item(inv_items), last_item: last_item(inv_items)}
   end
